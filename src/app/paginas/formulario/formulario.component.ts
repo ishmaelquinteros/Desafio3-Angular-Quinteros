7ff62a9d92536a5cc6a1c869445025c5f2df1e08
Ismael Quinteros <ishmaelquinteros@gmail.com>
2022-12-08 15:39:37 -0300

    Finalizado

--- src/app/paginas/formulario/formulario.component.ts
+++ src/app/paginas/formulario/formulario.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent implements OnInit {
+export class FormularioComponent {
+  nombreControl = new FormControl(
+    '',[Validators.required, Validators.minLength(4)]
+  );
+  apellidoControl = new FormControl(
+    '',[Validators.required, Validators.minLength(4)]
+  );
+  emailControl = new FormControl(
+    '',[Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]
+  );
+  passControl = new FormControl(
+    '',[Validators.required, Validators.minLength(6), Validators.maxLength(10), Validators.pattern("^[A-Za-z0-9#&]+$")]
+  );
 
-  constructor() { }
-
-  ngOnInit(): void {
+  formRegistar = new FormGroup({
+    nombreUsuario: this.nombreControl,
+    apellidoUsuario: this.apellidoControl,
+    emailUsuario: this.emailControl,
+    passUsuario: this.passControl
+  })
+  onSubmit() {
+    console.log(this.formRegistar.value)
   }
-
 }
